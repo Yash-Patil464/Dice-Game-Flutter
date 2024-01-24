cd5dece264e4c53af9862a5912862f425a1fd12a
Yash-Patil464 <yash.patil.464@gmail.com>
2024-01-24 21:28:36 +0530

    test

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,27 +1,26 @@
+import 'package:dice_game/dice_roller.dart';
 import 'package:flutter/material.dart';
-import 'package:dice_game/styledtext.dart';
 
 const startAlignment = Alignment.topLeft;
 const endAlignment = Alignment.bottomRight;
 
 class GradientContainer extends StatelessWidget {
-  const GradientContainer({super.key});
+  const GradientContainer(this.color1, this.color2, {super.key});
+  final Color color1, color2;
 
   @override
   Widget build(context) {
     return Container(
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            colors:  [ Color.fromARGB(255, 26, 2, 80),
-              Color.fromARGB(255, 45, 7, 98),
-            ],
-            begin: startAlignment,
-            end: endAlignment,
-          ),
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [color1, color2],
+          begin: startAlignment,
+          end: endAlignment,
         ),
-        child: const Center(
-          child: StyledText(),
-        ),
+      ),
+      child: const Center(
+        child: DiceRoller()
+      ),
     );
   }
 }
--- lib/styledtext.dart
+++ lib/styledtext.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 
-class StyledText extends StatelessWidget{
-  const StyledText({super.key});
+class StyledText extends StatelessWidget {
+  const StyledText(this.txt, {super.key});
+  final String txt;
 
   @override
-  Widget build(context){
-    return const Text("Hello World", style: TextStyle(
+  Widget build(context) {
+    return Text(
+      txt,
+      style: const TextStyle(
         color: Colors.white,
         fontSize: 28,
       ),
